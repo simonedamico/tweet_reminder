05bbaca3b61d2d662f3d30a43e74f2ecf767bc4c
Simone D'Amico <sim@me.com>
2011-06-30 21:26:26 +0200

    if interrupted with ctrl-c exit

--- lib/tweet_reminder.rb
+++ lib/tweet_reminder.rb
@@ -1,3 +1,2 @@
 require 'tweet_reminder/bot'
-require 'tweet_reminder/tweet_fetcher'
-require 'tweet_reminder/tweet_sender'
+
--- lib/tweet_reminder/bot.rb
+++ lib/tweet_reminder/bot.rb
@@ -1,4 +1,5 @@
 require 'yaml'
+require 'twitter'
 
 module TweetReminder
   class Bot
@@ -7,16 +8,37 @@ module TweetReminder
     
     def initialize(output)
       @output = output
+      @interrupted = false
+      trap("INT") { @interrupted = true }
     end
     
     def start
       load_conf
+      setup_twitter
+      loop do
+        fetch_tweets
+        
+        exit if @interrupted
+      end
     end
     
     def load_conf
       yaml = YAML::load(File.open('conf/bot.yaml'))
       @twitter_credentials = yaml['twitter']
       @output.puts('Configuration loaded.')
+    end
+    
+    def setup_twitter
+      Twitter.configure do |config|
+        config.consumer_key = @twitter_credentials['consumer_key']
+        config.consumer_secret = @twitter_credentials['consumer_secret']
+        config.oauth_token = @twitter_credentials['oauth_token']
+        config.oauth_token_secret = @twitter_credentials['oauth_secret']
+      end
+    end
+    
+    def fetch_tweets
+      
     end
   end
 end
